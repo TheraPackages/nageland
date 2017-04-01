081ce474fea43bcfae446affe1608d065df750d9
dengxu <hzdengxu@163.com>
2017-04-01 19:38:58 +0800

    add mark

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 
-var Markdown = require('markdown-to-html').Markdown;
+var Markdown = require('./lib/markdown').Markdown;
+console.log(Markdown);
 var md = new Markdown();
-md.bufmax = 2048;
+md.bufmax = 4096;
 var fileName = process.argv[2];
-var opts = {}; 
+var opts = {};
+
 md.once('end', function() {
 });
+
 md.render(fileName, opts, function(err) {
+
   if (err) {
     console.error('>>>' + err);
     process.exit();
   }
-  md.pipe(process.stdout);
+
+  console.log(md.html);
+  // md.pipe(process.stdout);
 });
--- zipreader.js
+++ zipreader.js
@@ -1,66 +1,34 @@
 var StreamZip = require('node-stream-zip');
 
-var zip = new StreamZip({
-    file: 'doc.zip',
+(function () {
+  var file = process.argv[2];
+
+  if (!file) {
+    console.error("[Zipreader] faid....");
+    return;
+  }
+
+  var zip = new StreamZip({
+    file: file,
     storeEntries: true
-});
+  });
 
-zip.on('error', function(err) {
-  /*handle*/
-});
+  zip.on('error', function(err) {
+    /*handle*/
+  });
 
-zip.on('ready', function() {
+  zip.on('ready', function() {
     console.log('Entries read: ' + zip.entriesCount);
     var entries = zip.entries();
 
-    for (var a in entries) {
-      if (!entries[a].isDirectory) {
-        if ("o/aa.zip" == entries[a].name) {
-          continue;
-        }
+    // 读取manifest文件
+    var manifest = zip.entryDataSync("doc/MANIFEST.json").toString('utf8');
+    var arrWeList = (manifest = JSON.parse(manifest))['.source.we'];
 
-        var fileName = entries[a].name;
-        var pos = fileName.lastIndexOf('/');
-        if (pos != -1) {
-          fileName = fileName.substring(pos + 1);
-        }
-
-        console.log("## "+ fileName);
-
-        var actualEentryData = zip.entryDataSync(entries[a].name).toString('utf8');
-        console.log(actualEentryData);
-      }
+    for (var i in arrWeList) {
+      console.log(arrWeList[i]);
+      var data = zip.entryDataSync("doc/" + arrWeList[i]).toString('utf8');
+      console.log(JSON.stringify(data));
     }
-
-    // stream to stdout
-    // zip.stream('node/benchmark/net/tcp-raw-c2s.js', function(err, stm) {
-    //     stm.pipe(process.stdout);
-    // });
-    // // extract file
-    // zip.extract('node/benchmark/net/tcp-raw-c2s.js', './temp/', function(err) {
-    //     console.log('Entry extracted');
-    //     consoe.log("djwjdiowjoi!!!!!!jdwdwjidjiwoidiw777");
-    // });
-    // // extract folder
-    // zip.extract('node/benchmark/', './temp/', function(err, count) {
-    //     console.log('Extracted ' + count + ' entries');
-    //     consoe.log("djwjdiowjoi!!!!!!jdwidiw777");
-    // });
-    // // extract all
-    // zip.extract(null, './temp/', function(err, count) {
-    //     console.log('Extracted ' + count + ' entries');
-    //     consoe.log("djwjdiowjoi!!!!!!");
-    // });
-    // // read file as buffer in sync way
-    // var data = zip.entryDataSync('README.md');
-    // consoe.log("djwjdiowjoi");
-});
-
-// zip.on('extract', function(entry, file) {
-    // console.log('Extracted ' + entry.name + ' to ' + file);
-// });
-// zip.on('entry', function(entry) {
-    // called on load, when entry description has been read
-    // you can already stream this entry, without waiting until all entry descriptions are read (suitable for very large archives)
-    // console.log('Read entry ', entry.name);
-// });
+  });
+})();
